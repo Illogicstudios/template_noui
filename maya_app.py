85fb1184744a581a87682694f2c3f1745423cf01
mariusjenin <marius2401@laposte.net>
2023-02-28 10:39:07 +0100

    template no UI done

---
+++ maya_app.py
@@ -0,0 +1,4 @@
+
+def run():
+    # Do something
+    pass
---
+++ template_main.py
@@ -0,0 +1,25 @@
+import sys
+import importlib
+
+if __name__ == '__main__':
+    # TODO specify the right path
+    install_dir = 'PATH/TO/template_noui'
+    if not sys.path.__contains__(install_dir):
+        sys.path.append(install_dir)
+
+    # TODO import right modules
+    modules = [
+        "maya_app"
+    ]
+
+    from utils import *
+    unload_packages(silent=True, packages=modules)
+
+    for module in modules:
+        importlib.import_module(module)
+
+    # TODO import the app
+    from maya_app import *
+
+    # TODO rename app variable
+    maya_app.run()
